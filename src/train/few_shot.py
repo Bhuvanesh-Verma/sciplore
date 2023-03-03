ea81e0a88f7dcbc3b57c6ba50119ce3931faf24e
Bhuvanesh Verma <workoholic24x7@gmail.com>
2023-03-03 19:56:00 +0100

    add few shot experiments and results

--- src/train/few_shot.py
+++ src/train/few_shot.py
@@ -1,50 +1,39 @@
 import argparse
-import json
-from collections import defaultdict
 
-import datasets
-import evaluate
 import numpy as np
-import torch
 import yaml
 from datasets import Dataset
 from sentence_transformers.losses import CosineSimilarityLoss
 from setfit import SetFitModel, SetFitTrainer
-from sklearn.model_selection import StratifiedShuffleSplit
 from sklearn.metrics import f1_score, accuracy_score, classification_report
-from src.train.base_models import str2sections, get_section_text
+
+from src.data.sci_dataset import SciDataset
+
 
 def compute_metrics(y_pred, y_test):
     return {
-        "f1": f1_score(y_test,y_pred.cpu().data.numpy(), average='macro'),
+        "f1": f1_score(y_test, y_pred.cpu().data.numpy(), average='macro'),
         "accuracy": accuracy_score(y_test, y_pred.cpu().data.numpy()),
         "y_pred": y_pred.cpu().data.numpy()
     }
-def pipeline(text, labels, config):
-    label2idx = {label:i for i, label in enumerate(set(labels))}
-    idx2label = {i:label for label, i in label2idx.items()}
 
-    n_splits = 5  # number of splits to create
-    sss = StratifiedShuffleSplit(n_splits=n_splits, test_size=0.2, random_state=42)
 
+def pipeline(dataset, data_type, config):
+    idx2label = dataset.idx2label
     f1s = []
     accs = []
-    model = SetFitModel.from_pretrained("sentence-transformers/paraphrase-mpnet-base-v2",
-                                        use_differentiable_head=True,
-                                        head_params={"out_features": 3},
-                                        )
-    # Split the data into training and testing sets
-    for i, (train_index, test_index) in enumerate(sss.split(text, labels)):
-        print(f'\n-------Split {i+1}-------')
-        X_train, X_test = [text[j] for j in train_index], [text[j] for j in test_index]
-        y_train, y_test = torch.tensor([label2idx[labels[j]] for j in train_index]), torch.tensor([label2idx[labels[j]] for j in test_index])
-        train_dataset = {'text': X_train, 'label': y_train}
 
+    # Split the data into training and testing sets
+    for i, (X_train, y_train, X_test, y_test) in enumerate(dataset.get_train_test_split(data_type,label_type='numeric', n_splits=5)):
+        print(f'\n-------Split {i + 1}-------')
+
+        train_dataset = {'text': X_train, 'label': y_train}
         train_dataset = Dataset.from_dict(train_dataset)
 
         test_dataset = {'text': X_test, 'label': y_test}
         test_dataset = Dataset.from_dict(test_dataset)
 
+        model = SetFitModel.from_pretrained(config['model_name'])
 
         # Create trainer
         trainer = SetFitTrainer(
@@ -55,7 +44,7 @@ def pipeline(text, labels, config):
             metric=compute_metrics,
             batch_size=8,
             num_iterations=15,  # Number of text pairs to generate for contrastive learning
-            num_epochs=3  # Number of epochs to use for contrastive learning
+            num_epochs=2  # Number of epochs to use for contrastive learning
         )
         # Train and evaluate!
         trainer.train()
@@ -66,9 +55,8 @@ def pipeline(text, labels, config):
         accs.append(metrics['accuracy'])
         f1s.append(metrics['f1'])
 
-
-    stats = {'best_accuracy': np.around(accs[np.argmax(accs)],2),
-             'avg_accuracy': np.around(np.mean(accs),2),
+    stats = {'best_accuracy': np.around(accs[np.argmax(accs)], 2),
+             'avg_accuracy': np.around(np.mean(accs), 2),
              'best_f1': np.around(accs[np.argmax(f1s)], 2),
              'avg_f1': np.around(np.mean(f1s), 2),
              }
@@ -95,24 +83,8 @@ if __name__ == '__main__':
     with open(args.config) as file:
         config_data = yaml.safe_load(file)
 
-    data_path = config_data['data_path']
-    with open(data_path) as f:
-        data = json.load(f)
-
-    new_data = str2sections(data)
+    dataset = SciDataset()
     data_type = args.data_type
-    labels = data['label']
-    text = None
-    if data_type == 'abstract':
-        text = [sec2text['Abstract'] for sec2text in new_data]
-    elif data_type == 'full_text':
-        text = data['full_text']
-    elif data_type == 'sec-name':
-        text = [' '.join(secs) for secs in data['sections']]
-    elif data_type == 'sec-text':
-        text = get_section_text(new_data)
-    else:
-        ValueError(f'Incorrect data type {data_type}')
 
-    stats = pipeline(text, labels, config_data)
+    stats = pipeline(dataset, data_type, config_data)
     print(stats)
